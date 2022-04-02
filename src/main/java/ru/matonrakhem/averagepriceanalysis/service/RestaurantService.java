2d64981a323bfa81efb07f9b48606f3281de5503
NikoalevRustem <nikolaevrustem13@gmail.com>
2022-04-02 18:55:33 +0300

    Добавил метод для рассчета среднего значения рейтинга в классе RestaurantService

--- src/main/java/ru/matonrakhem/averagepriceanalysis/service/RestaurantService.java
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/service/RestaurantService.java
@@ -1,8 +1,50 @@
 package ru.matonrakhem.averagepriceanalysis.service;
 
 import org.springframework.stereotype.Service;
+import ru.matonrakhem.averagepriceanalysis.model.RestaurantModel;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class RestaurantService {
+    private long id = 0;
+    private List<RestaurantModel> restaurantModelList = new ArrayList<>();
+    private List<RestaurantModel> tempRestaurantModelList = new ArrayList<>();
 
+    public List<RestaurantModel> getRestaurantModelList() {
+        return restaurantModelList;
+    }
+
+    public void saveRestaurantList(RestaurantModel model) {
+        model.setId(id);
+        restaurantModelList.add(model);
+        id++;
+    }
+
+    public double calcAverageRaiting(String city) {
+        double tempRaiting;
+        double sumRaiting = 0;
+        double avgRaiting;
+        int count = 0;
+        List<Double> allRaitingRestaurant = new ArrayList<>();
+
+        for (RestaurantModel restaurantModel : restaurantModelList) {
+            if (restaurantModel.getCity().equals(city)) {
+                tempRestaurantModelList.add(restaurantModel);
+            }
+        }
+        for (RestaurantModel temporaryRestourantModel : tempRestaurantModelList) {
+            allRaitingRestaurant.add(temporaryRestourantModel.getRaiting());
+        }
+        for (int i = 0; i <= allRaitingRestaurant.size(); i++) {
+            count++;
+            tempRaiting = allRaitingRestaurant.get(i);
+            sumRaiting = tempRaiting + sumRaiting;
+        }
+        avgRaiting = sumRaiting / count;
+
+        return avgRaiting;
+    }
 }
+
