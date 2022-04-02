321e1bb47e7b0ab9954e52e5d0d95f7aba5556e1
vladislavmatveev <yasololinestoyalvsegda@gmail.com>
2022-04-02 16:40:27 +0300

    Добавили главную страничку

---
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/controller/AverageController.java
@@ -0,0 +1,13 @@
+package ru.matonrakhem.averagepriceanalysis.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AverageController {
+
+    @GetMapping("/")
+    public String mainPage(){
+        return "mainPage";
+    }
+}
