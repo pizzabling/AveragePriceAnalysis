93ce472008fdb4d5b82634fd0b38269d107bb0c8
NikoalevRustem <nikolaevrustem13@gmail.com>
2022-04-02 17:16:22 +0300

    Создал модель ресторана

--- src/main/java/ru/matonrakhem/averagepriceanalysis/model/RestaurantModel.java
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/model/RestaurantModel.java
@@ -1,5 +1,18 @@
 package ru.matonrakhem.averagepriceanalysis.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class RestaurantModel {
 
+    private long id;
+    private double raiting;
+    private String city;
+    private String name;
 }
