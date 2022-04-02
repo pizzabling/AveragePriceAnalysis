cdad30b8baedc9aa410c993b8837006ac8434c72
vladislavmatveev <yasololinestoyalvsegda@gmail.com>
2022-04-02 16:21:09 +0300

    Initial commit

---
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/AveragePriceAnalysisApplication.java
@@ -0,0 +1,13 @@
+package ru.matonrakhem.averagepriceanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AveragePriceAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AveragePriceAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/matonrakhem/averagepriceanalysis/AveragePriceAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.matonrakhem.averagepriceanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AveragePriceAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
