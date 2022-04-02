ece9b2b951b49777edb2596ac157f94081ab2219
vladislavmatveev <yasololinestoyalvsegda@gmail.com>
2022-04-02 17:55:53 +0300

    В класс PizzaService добавил комменты к действиям

--- src/main/java/ru/matonrakhem/averagepriceanalysis/service/PizzaService.java
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/service/PizzaService.java
@@ -11,38 +11,50 @@ public class PizzaService {
     private List<PizzaModel> pizzaModelList = new ArrayList<>();
     private List<PizzaModel> tempPizzaModel = new ArrayList<>();
     private long id = 0;
+
+    /*Берём список пицц*/
     public List<PizzaModel> getPizzaList(){
         return pizzaModelList;
     }
-
+    /*Сейвим пиццу*/
     public void savePizzaList(PizzaModel model){
         model.setId(id);
         pizzaModelList.add(model);
         id++;
     }
-
-    public void calcAverage(String city, String pizzaName){
+    /*Считаем средний ценник по пиццам. Фильтр: город, название пиццы*/
+    public double calcAveragePrice(String city, String pizzaName){
         double tempPrice;
         double sumPrice = 0;
         double avgPrice;
         int count = 0;
+        /*Объявляем новый список с ценниками пицц*/
         List<Double> allPricePizza = new ArrayList<>();
+
+        /*Вытаскиваем пиццы, которые подходят под условия фильтра*/
         for (PizzaModel pizzaModel : pizzaModelList){
             if(pizzaModel.getPizzaName().equals(pizzaName) && pizzaModel.getCity().equals(city)){
                 tempPizzaModel.add(pizzaModel);
             }
         }
+
+        /*Вытаскиваем все ценники пицц, которые попали под условия фильтра*/
         for (PizzaModel tempraryPizzaModel : tempPizzaModel){
             allPricePizza.add(tempraryPizzaModel.getPrice());
         }
-
+        /*Рассчитываем сумму пицц, которые попали под условия фильтра */
         for(int i = 0; i <= allPricePizza.size(); i++){
             count++;
             tempPrice = allPricePizza.get(i);
             sumPrice = tempPrice + sumPrice;
         }
-
+        /*Считаем средний ценник пицц, которые попали под условия фильтра*/
         avgPrice = sumPrice / count;
 
+        /*Возвращаем средний ценник*/
+        return avgPrice;
     }
+
+
+
 }
