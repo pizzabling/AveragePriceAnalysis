58c380b221bb6c2df39658785ceaa30a0dabf145
NikoalevRustem <nikolaevrustem13@gmail.com>
2022-04-03 16:57:08 +0300

    Добавил суши сервис

--- src/main/java/ru/matonrakhem/averagepriceanalysis/model/SushiModel.java
+++ src/main/java/ru/matonrakhem/averagepriceanalysis/model/SushiModel.java
@@ -1,5 +1,21 @@
 package ru.matonrakhem.averagepriceanalysis.model;
 
-public class SushiModel {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class SushiModel {
+    private long id;
+    private double price;
+    private double weight;
+    private double rating;
+    private String sushiName;
+    private String restaurantName;
+    private String city;
 }
+
